1c6d2e7b53843eca365d12d6f966a73952fec737
tcouloumy <t.couloumy@gmail.com>
2022-02-12 18:21:26 +0100

    router file style

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,15 @@
-import Search from '@/views/Search.vue';
+import Search from '../views/Search.vue';
 import ForecastDetails from '../views/ForecastDetails.vue';
-import { DEFAULT_LANGUAGE, SUPPORTED_LANGUAGES } from '@/constants/language';
-import { i18n } from '@/plugins/i18n';
+import { DEFAULT_LANGUAGE, SUPPORTED_LANGUAGES } from '../constants/language';
+import { i18n } from '../plugins/i18n';
 
 export default [{
   path: '/',
-  redirect: `/${DEFAULT_LANGUAGE}`,
-  	}, {
+  redirect: `/${DEFAULT_LANGUAGE}`
+}, {
   path: '/:lang',
   component: {
-    template: '<router-view></router-view>',
+    template: '<router-view></router-view>'
   },
   beforeEnter(to, from, next) {
     const { lang } = to.params;
@@ -18,21 +18,25 @@ export default [{
     if (i18n.locale === lang) return next();
 
     // Async load of translation file
-    import(`@/lang/${lang}.json`).then((locales) => {
-      i18n.setLocaleMessage(lang, locales.default || locales);
-      i18n.locale = lang;
-      return next();
-    });
+    import(`@/lang/${lang}.json`)
+      .then((locales) => {
+        i18n.setLocaleMessage(lang, locales.default || locales);
+        i18n.locale = lang;
+
+        return next();
+      });
+
+    return null;
   },
   children: [{
     path: '',
     name: 'Search',
-    component: Search,
+    component: Search
   },
   {
     path: 'forecast/:locationString',
     name: 'Forecast',
-    component: ForecastDetails,
-  }],
-},
+    component: ForecastDetails
+  }]
+}
 ];
