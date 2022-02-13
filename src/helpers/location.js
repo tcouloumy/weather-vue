593d86425ee37474cbf5b85a12b479ff9a94d069
tcouloumy <t.couloumy@gmail.com>
2022-02-13 12:42:18 +0100

    helpers

--- src/helpers/location.js
+++ src/helpers/location.js
@@ -1,8 +1,9 @@
 /**
-* Return a formated location string for URLs
-* @param {Object} loc Location object containing latitude and longitude
-* @param {int} long Longitude
-*/
+  * Return a formated location string for URLs
+  *
+  * @param {Object} loc Location object containing latitude and longitude
+  * @param {int} long Longitude
+  */
 export function locationToString(loc) {
   return `${loc.latitude.toString()},${loc.longitude.toString()}`;
 }
@@ -17,19 +18,21 @@ export function stringToLocation(string) {
 
   return {
     latitude: parseFloat(location[0]),
-    longitude: parseFloat(location[1]),
+    longitude: parseFloat(location[1])
   };
 }
 
 /**
-* Takes a result json from Google geoloc API and return a json formatted like the Autocomplete place API
-* @param {Object} geoloc Result from Geoloc API
-* @return {Object} Formatted JSON
-*/
-export function parseReverseGeocodeResult(geoloc) {
+  * Takes a result json from Google geoloc API and
+  * return a json formatted like the Autocomplete place API
+  *
+  * @param {Object} geoloc Result from Geoloc API
+  * @return {Object} Formatted JSON
+  */
+export function parseReverseGeocodeResult(geoloc) {
   const result = {};
 
-  geoloc.data.results[0].address_components.map((item) => {
+  geoloc.data.results[0].address_components.forEach((item) => {
     // We need the country's full name
     if (item.types[0] === 'country') result[item.types[0]] = item.long_name;
     else result[item.types[0]] = item.short_name;
@@ -38,24 +41,25 @@ export function parseReverseGeocodeResult(geoloc) {
   return {
     ...result,
     latitude: geoloc.data.results[0].geometry.location.lat,
-    longitude: geoloc.data.results[0].geometry.location.lng,
+    longitude: geoloc.data.results[0].geometry.location.lng
   };
 }
 
 /**
-* First level key-value comparison two locations object
-* Because of a float precision issues and difference in API responses, we'll need to compare only
-* specific properties of the objects
-* @param {Float} loc1 First location
-* @param {Float} loc2 Location to compare to
-* @return {Boolen} Object are equals or not
-*/
+  * First level key-value comparison two locations object
+  * Because of a float precision issues and difference in API responses, we'll need to compare only
+  * specific properties of the objects
+  *
+  * @param {Float} loc1 First location
+  * @param {Float} loc2 Location to compare to
+  * @return {Boolen} Object are equals or not
+  */
 export function areEquals(loc1, loc2) {
   if (!loc1 || !loc2 || !loc1.locality || !loc2.locality) return false;
 
   // We need to remove the accented character because they depends on the locale
   // and limite the decimals of the coordinates to avoir the issues of float precision
   return loc1.locality.normalize('NFD').replace(/[\u0300-\u036f]/g, '') === loc2.locality.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-		   && loc1.latitude.toFixed(4) === loc2.latitude.toFixed(4)
-		   && loc1.longitude.toFixed(4) === loc2.longitude.toFixed(4);
+    && loc1.latitude.toFixed(4) === loc2.latitude.toFixed(4)
+    && loc1.longitude.toFixed(4) === loc2.longitude.toFixed(4);
 }
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import VueI18 from 'vue-i18n';
-import { DEFAULT_LANGUAGE, FALLBACK_LANGUAGE } from '@/constants/language';
-import en from '@/lang/en.json';
+import { DEFAULT_LANGUAGE, FALLBACK_LANGUAGE } from '../constants/language';
+import en from '../lang/en.json';
 
 Vue.use(VueI18);
+
 export const i18n = new VueI18({
   locale: DEFAULT_LANGUAGE,
   fallbackLocale: FALLBACK_LANGUAGE,
-  messages: { en },
+  messages: { en }
 });
+
+export default i18n;
