63fe16f6e99dfd861cac533b995f51e86b790482
tcouloumy <t.couloumy@gmail.com>
2022-02-13 12:39:24 +0100

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,25 @@
+/* eslint-disable no-param-reassign */
+
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { areEquals } from '@/helpers/location';
+import { areEquals } from '../helpers/location';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    favoriteLocation: [],
+    favoriteLocation: []
   },
   mutations: {
     ADD_FAVORITE(state, payload) {
       state.favoriteLocation.push(payload);
     },
     REMOVE_FAVORITE(state, payload) {
-      state.favoriteLocation = state.favoriteLocation.filter((item, index) => !areEquals(item, payload));
+      state.favoriteLocation = state
+        .favoriteLocation
+        .filter((item) => !areEquals(item, payload));
     },
-    SORT_FAVORITE(state, sortKey) {
+    SORT_FAVORITE(state) {
       state.favoriteLocation.sort((a, b) => {
         if (a.locality < b.locality) {
           return -1;
@@ -25,13 +29,14 @@ export default new Vuex.Store({
         }
         return 0;
       });
-    },
+    }
   },
   actions: {
     /**
-		* Initialise store with local storage data if it exists
-		* @param {Object} context Current store context
-		*/
+      * Initialise store with local storage data if it exists
+      *
+      * @param {Object} context Current store context
+      */
     initialiseStore(context) {
       if (localStorage.getItem('favoriteLocation')) {
         Object.assign(context.state.favoriteLocation, JSON.parse(localStorage.getItem('favoriteLocation')));
@@ -39,10 +44,11 @@ export default new Vuex.Store({
     },
 
     /**
-		* Add a location to the favorite it doesn't exists, removes if it does
-		* @param {Object} context Current store context
-		* @param {Object} location Location object payload
-		*/
+      * Add a location to the favorite it doesn't exists, removes if it does
+      *
+      * @param {Object} context Current store context
+      * @param {Object} location Location object payload
+      */
     toggleFavorite(context, location) {
       if (context.state.favoriteLocation.findIndex((item) => areEquals(item, location)) !== -1) {
         context.commit('REMOVE_FAVORITE', location);
@@ -50,6 +56,6 @@ export default new Vuex.Store({
         context.commit('ADD_FAVORITE', location);
         context.commit('SORT_FAVORITE', location);
       }
-    },
-  },
+    }
+  }
 });
