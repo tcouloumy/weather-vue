75f57249a8bfd46839556d8f04740089eed0d6f2
tcouloumy <t.couloumy@gmail.com>
2022-02-13 15:25:54 +0100

    🐛 Language switch not working from en to fr (#24)

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -2,13 +2,14 @@ import Vue from 'vue';
 import VueI18 from 'vue-i18n';
 import { DEFAULT_LANGUAGE, FALLBACK_LANGUAGE } from '../constants/language';
 import en from '../lang/en.json';
+import fr from '../lang/fr.json';
 
 Vue.use(VueI18);
 
 export const i18n = new VueI18({
   locale: DEFAULT_LANGUAGE,
   fallbackLocale: FALLBACK_LANGUAGE,
-  messages: { en }
+  messages: { en, fr }
 });
 
 export default i18n;
